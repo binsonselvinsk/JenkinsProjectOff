05db2b955a2c2d8357c811a89b65d3b20038b461
binsonselvinsk <binson.selvin@skinternational.com>
2024-07-15 10:18:15 +0530

    initial project structure commit

---
+++ src/main/java/com/sk/JenkinsProjectOff/JenkinsProjectOffApplication.java
@@ -0,0 +1,13 @@
+package com.sk.JenkinsProjectOff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsProjectOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsProjectOffApplication.class, args);
+	}
+
+}
